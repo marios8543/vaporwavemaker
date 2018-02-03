f9ae1b26c73ebac5c4cb145cf7eb05a52264d32f
marios <marios8543@gmail.com>
2018-02-03 15:53:27 +0200

    Rewrite of index and vaporwave_mp3
    Complete overhaul of the code. Now uses MySQL to store song names and other data instead of txt files. Also much more clean and comprehensive code...

--- vaporwave_mp3.php
+++ vaporwave_mp3.php
@@ -1,62 +1,49 @@
-<center>
-<font size="6"><font color="4afafc"><h1 style="background-color:#ff67f2">V A P O R W A V E  &nbsp;  M A K E R</h1></font>
-<body background="watervap.jpg" bgproperties="fixed">
 <?php
 
-	if(isset($_FILES['song'])){
-		$errors= array();
-		$file_name = $_FILES['song']['name'];
-		$file_size =$_FILES['song']['size'];
-		$file_tmp =$_FILES['song']['tmp_name'];
-		$file_type=$_FILES['song']['type'];
-		$file_ext = end((explode(".",$file_name)));
-		
-		var_dump($_FILES['song']);
-		$song_id = '';
-        for ($i = 0; $i<10; $i++) 
+function get_client_ip() {
+    $ipaddress = '';
+    if (getenv('HTTP_CLIENT_IP'))
+        $ipaddress = getenv('HTTP_CLIENT_IP');
+    else if(getenv('HTTP_X_FORWARDED_FOR'))
+        $ipaddress = getenv('HTTP_X_FORWARDED_FOR');
+    else if(getenv('HTTP_X_FORWARDED'))
+        $ipaddress = getenv('HTTP_X_FORWARDED');
+    else if(getenv('HTTP_FORWARDED_FOR'))
+        $ipaddress = getenv('HTTP_FORWARDED_FOR');
+    else if(getenv('HTTP_FORWARDED'))
+       $ipaddress = getenv('HTTP_FORWARDED');
+    else if(getenv('REMOTE_ADDR'))
+        $ipaddress = getenv('REMOTE_ADDR');
+    else
+        $ipaddress = 'UNKNOWN';
+    return $ipaddress;
+}
+$conn = mysqli_connect("localhost","root","","vaporwavemaker");
+$max_size = 15728640;
+$allowed_exts = ['mp3','wav','wma','flac'];
+$song_id = 0;
+for ($i = 0; $i<10; $i++)
         {
         $song_id .= mt_rand(0,9);
         }
-		$expensions= array("mp3",); 		
-		if(in_array($file_ext,$expensions)=== false){
-			$errors[]="extension not allowed, please choose an mp3 file.";
-		}
-		if($file_size > 10485760){
-		$errors[]='File size must not be larger than 10MB';
-		}				
-		if(empty($errors)==true){
-			$song_name = $song_id.'.'.$file_ext;
-			var_dump($song_name);
-			move_uploaded_file($file_tmp,"songs/".$song_name);
-			setcookie('song_id_str', $song_id);
-			file_put_contents("songs_id/".$song_id.'.txt', $file_name);
-			var_dump($file_ext);
-			$item='example';
-			$tmp = exec('sox.exe'.' '.'C:\wamp64\www\songs\\'.$song_id.'.mp3'.' '.'C:\wamp64\www\songs_final\\'.$song_id.'.mp3'.' '.'speed 0.8 reverb 80 50 100 100 0 0' );
-			$errors = $tmp;
-			$songexist_name = 'C:\wamp64\www\songs_final\\'.$song_id.'.mp3';
-            $song_exists = file_exists($songexist_name);
-			if($song_exists === true){
-			$redir_link = '/player.php?songid='.$song_id;
-			header("Location:".' '.$redir_link);
-
-		
-            exit();
-			}
-			else{
-				echo $tmp;
-				?>
-				
-                    <h2>Oops that didn't go as planned :/</h2>
-<?php					
-					echo $errors;
-			}
-		}else{
-			?>
-			    <h2>Oops that didn't go as planned :/</h2>
-				<?php
-				echo $errors;
-				echo $tmp;
-		}
-	}
-?>
+if(isset($_FILES['song'])){
+	$song_name = $_FILES['song']['name'];
+	$song_type = $_FILES['song']['type'];
+	$song_size = $_FILES['song']['size'];
+	$song_tmp = $_FILES['song']['tmp_name'];
+	$song_error = $_FILES['song']['error'];
+	$song_ext = explode('/',$song_type);
+	$song_ext = end($song_ext);
+  $user_ip = get_client_ip();
+	if(in_array($song_ext,$allowed_exts) && $song_size<=$max_size){
+    if($conn->query("INSERT INTO songs(song_id,song_name,size,type,user_ip) values('$song_id','$song_name','$song_size','$song_type','$user_ip')")){
+      move_uploaded_file($song_tmp,"C:\\wamp64\\tmp\\$song_id.$song_ext");
+      $tmp = exec("sox C:\\wamp64\\tmp\\$song_id.$song_ext C:\\wamp64\\www\Songs\\$song_id.mp3 speed 0.8 reverb 80 50 100 100 0 0");
+      header("Location:/player.php?song=$song_id");
+    }
+    echo '<center><h1>Something went wrong. Please try again or come back later...</h1></center>';
+}
+echo "<center><h1> The format of your song isn't supported or it's too large. Make sure it's an mp3,wav or wma file no larger than 15MB </center></h1>";
+}
+echo $conn->error;
+ ?>
